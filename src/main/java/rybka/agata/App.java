d178326a532661d0780f85790ce2d8b99db6cf30
AtkaR <agata-rybak@o2.pl>
2017-04-21 14:40:54 +0200

    App1 V1

---
+++ src/main/java/rybka/agata/App.java
@@ -0,0 +1,15 @@
+package rybka.agata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println("RYBKA 1");
+    }
+}
