2e03077117be6af4185080e1374cd136a7060a03
AtkaR <agata-rybak@o2.pl>
2017-04-21 14:49:45 +0200

    App1 V6

--- src/main/java/rybka/agata/Person.java
+++ src/main/java/rybka/agata/Person.java
@@ -11,19 +11,4 @@ public class Person {
     public Person(String name) {
         this.name = name;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
