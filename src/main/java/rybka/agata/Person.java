a990d70674dfe94b780035c079f405177197f0ff
AtkaR <agata-rybak@o2.pl>
2017-04-21 14:41:55 +0200

    App1 V2

---
+++ src/main/java/rybka/agata/Person.java
@@ -0,0 +1,14 @@
+package rybka.agata;
+
+/**
+ * Created by asus on 2017-04-21.
+ */
+public class Person {
+    private String name;
+
+    public Person(){}
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
